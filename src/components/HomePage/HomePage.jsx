ec15958d63de26f513ac1772b85f237a93f05672
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-09 22:09:57 +0600

    Email and Password Authorization Repository Added

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h2 className='text-3xl font-semibold'>Welcome to email and password auth home</h2>
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from './layout/MainLayout.jsx';
+import HomePage from './components/HomePage/HomePage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage></HomePage>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
